d4b80a650a511342cce0e99b2b7af1192f2e3cbd
João Paulo Penney <137862398+joaopaulopenney@users.noreply.github.com>
2023-08-08 03:00:36 -0300

    Aulas 3 a 6

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
+import SayMyName from './components/SayMyName'
+import Pessoa from './components/Pessoa'
+import Frase from './components/Frase'
 
 function App() {
+  const nome = 'Maria'
+
   return (
     <div className="App">
-      <h1>Olá React!</h1>
-      <p>Meu primeiro App</p>
+      <h1>Testando CSS</h1>
+      <Frase />
+      <Frase />
+      <SayMyName nome="Matheus" />
+      <SayMyName nome="João" />
+      <SayMyName nome={nome} />
+      <Pessoa 
+      nome="Rodrigo" 
+      idade="28" 
+      profissao="Programador" 
+      foto="https://via.placeholder.com/150" />
     </div>
   )
 }
